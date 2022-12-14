fbe60806ee399a9a57c10d47745fb192c9f0a9ea
Samuele Meschini <samuele.meschini@polito.it>
2022-12-14 18:16:45 +0100

    rename slx file

--- fuelCycleSimulator.m
+++ fuelCycleSimulator.m
@@ -1,7 +1,7 @@
 clear all
 % Input data required for fuel cycle
 % Residence times [s]
-tau1 = 1*1200; % Blanket
+tau1 = 1.25*3600; % Blanket
 tau2 = 1*24*3600; % TES
 tau3 = 1000; % FW
 tau4 = 1000; % Divertor
@@ -11,6 +11,7 @@ tau7 = 600; % Vacuum pump
 tau8 = 0.9*3600; % Fuel clean-up - tau8+tau9 = 4h from Abdou's paper
 tau9 = 3.1*3600; % ISS -  tau8+tau9 = 4h from Abdou's paper
 tau10 = 1*3600;
+tau12 = 1*3600; % Membrane separation
 % Decay constant
 lambda = 1.73e-9; %12.33 y half-life
 % Non-radioactive losses fraction
@@ -25,15 +26,15 @@ T6 = 1/((1 + epsi)/tau6 + lambda); % Detritiation system
 T7 = 1/((1 + epsi)/tau7 + lambda); % Vacuum pump
 T8 = 1/((1 + epsi)/tau8 + lambda); % Fuel clean-up
 T9 = 1/((1 + epsi)/tau9 + lambda); % ISS
+T12 = 1/((1 + epsi)/tau12 + lambda);
 % Flow rate fractions
 
-
+% Flow rate fractions
 % From plasma to components
 fp3 = 1e-4;
 fp4 = 1e-4;
 
-%From component to component - Fractions from HX to components taken from
-% (Kuang, 2018)
+%From component to component
 f51 = 0.33; % HX to blanket
 f53 = 0.33; % HX to FW
 f56 = 1e-4; % HX to DS
@@ -42,14 +43,12 @@ f96 = 0.1; % ISS to DS
 f_dir = 0.3; % DIR fraction
 
 % Components' efficiency
-eta2 = 0.95; % TES
-eta6 = 0.95; % DS
+eta2 = 0.95;
+eta6 = 0.95;
 
 % General parameters
 N_dot = 8.99e-7; % Tritium burnt [kg/s]
 f_b = 0.02; % burnup fraction
-TBR = 1.08;
-I_s_0 = 1.5; %  startup inventory [kg]
 
 % Reserve inventory
 q = 0.25; % fraction of FC failing
@@ -60,10 +59,11 @@ t_d = 2;
 
 % Define model to be run
 model = "fuelCycle.slx";
-accuracy = 0.01; % accuracy when computing the required TBR 
+accuracy = 0.005; % accuracy when computing the required TBR 
 sim_time = 3 * 8760 * 3600; % simulation time [s]
 runMode = "single" % single or iteration
-TBR = 1.06; % TBR - If runMode = "single" this is fixed
+
+TBR = 1.08; % TBR - If runMode = "single" this is fixed
             %       If runMode = "iteration" this is the initial guess
 I_s_0 = 1.5; % startup inventory [kg] - If runMode = "single" this is fixed
              %                          If runMode = "iteration" this is the initial guess
@@ -82,8 +82,8 @@ if strcmp(runMode,"single")
     % Checking that I_st > 0 for every t once the 
     % simulation is done is a good idea!
     header = ['time[s]', 'blanket inventory[kg]', 'TES inventory [kg]', 'ISS inventory [kg]', 'storage inventory [kg]'];
-    writematrix(header,'results/results.csv');
-    writematrix([out.tout, out.I_1, out.I_2, out.I_9, out.I_11], 'results.csv', "WriteMode","append");
+    writematrix(header,'results/results.csv', "WriteMode","overwrite");
+    writematrix([out.tout, out.I_1, out.I_2, out.I_9, out.I_11], 'results/results.csv', "WriteMode","append");
 elseif strcmp(runMode,"iteration")
     %Iterative search for the required TBR and start-up inventory
     utilities_fc.find_tbr(I_s_0, I_reserve, t_d, TBR, model, accuracy)
