73dfb88e9c372ba27bc1630d6bb30eec92f8b466
Samuele Meschini <samuele.meschini@polito.it>
2022-12-15 13:36:18 +0100

    minor fixes

--- fuelCycleSimulator.m
+++ fuelCycleSimulator.m
@@ -8,33 +8,35 @@ model = "fuelCycle.slx";
 TBR_accuracy = 0.005; % accuracy when computing the required TBR 
 inventory_accuracy = 0.01;
 sim_time = 3 * 8760 * 3600; % simulation time [s]
-runMode = "single" % single or iteration
-run('parametric_analysis_TBE.m')
+runMode = "iteration" % single, iteration or parametric analysis
+parametric_variable = 'TBE';
 
-% TBR = 1.08; % TBR - If runMode = "single" this is fixed
-%             %       If runMode = "iteration" this is the initial guess
-% I_s_0 = 1.5; % startup inventory [kg] - If runMode = "single" this is fixed
-%              %                          If runMode = "iteration" this is the initial guess
-% 
-% 
-% % If you know the required TBR and the start-up inventory, run in "single"
-% % mode. If you don't know them, run in "iteration" mode, find the required
-% % TBR and the start-up inventory for a given configuration, and then run a
-% % "single" simulation to extract the inventories (and wathever variable you
-% % want) from the model. 
-% 
-% 
-% if strcmp(runMode,"single")
-%     %Single simulation with fixed TBR and start-up inventory
-%     out = sim(model); 
-%     % Checking that I_st > 0 for every t once the 
-%     % simulation is done is a good idea!
-%     header = ['time[s]', 'blanket inventory[kg]', 'TES inventory [kg]', 'ISS inventory [kg]', 'storage inventory [kg]'];
-%     writematrix(header,'results/results.csv', "WriteMode","overwrite");
-%     writematrix([out.tout, out.I_1, out.I_2, out.I_9, out.I_11], 'results/results.csv', "WriteMode","append");
-% elseif strcmp(runMode,"iteration")
-%     %Iterative search for the required TBR and start-up inventory
-%     utilities.find_tbr(I_s_0, I_reserve, t_d, TBR, model, accuracy)
-% else 
-%     fprintf("Enter a valid run mode \n")
-% end
+TBR = 1.08; % TBR - If runMode = "single" this is fixed
+            %       If runMode = "iteration" this is the initial guess
+I_s_0 = 1.5; % startup inventory [kg] - If runMode = "single" this is fixed
+             %                          If runMode = "iteration" this is the initial guess
+
+
+% If you know the required TBR and the start-up inventory, run in "single"
+% mode. If you don't know them, run in "iteration" mode, find the required
+% TBR and the start-up inventory for a given configuration, and then run a
+% "single" simulation to extract the inventories (and wathever variable you
+% want) from the model. 
+
+
+if strcmp(runMode,"single")
+    %Single simulation with fixed TBR and start-up inventory
+    out = sim(model); 
+    % Checking that I_st > 0 for every t once the 
+    % simulation is done is a good idea!
+    header = ['time[s]', 'blanket inventory[kg]', 'TES inventory [kg]', 'ISS inventory [kg]', 'storage inventory [kg]'];
+    writematrix(header,'results/results.csv', "WriteMode","overwrite");
+    writematrix([out.tout, out.I_1, out.I_2, out.I_9, out.I_11], 'results/results.csv', "WriteMode","append");
+elseif strcmp(runMode,"iteration")
+    %Iterative search for the required TBR and start-up inventory
+    utilities.find_tbr(I_s_0, I_reserve, t_d, TBR, model, TBR_accuracy, inventory_accuracy)
+elseif strcmp(runMode,'parametric')
+    run(strcat('parametric_analysis_',parametric_variable,'.m'))
+else
+    fprintf("Enter a valid run mode \n")
+end
--- parametric_analysis_TBE.m
+++ parametric_analysis_TBE.m
@@ -1,15 +1,16 @@
-TBR_start = 1.06; % TBR - If runMode = "single" this is fixed
-            %       If runMode = "iteration" this is the initial guess
-I_s_0_start = 1.5; % startup inventory [kg] - If runMode = "single" this is fixed
-             %                          If runMode = "iteration" this is the initial guess          
 TBE_array = [0.1, 0.5, 1, 2, 3, 4, 5, 10]/100; % TBE [-]
 for i=1:numel(TBE_array)
+    TBR = 1.06;           
+    I_s_0 = 0.8; 
+    TBE = TBE_array(i);
+    if TBE == 0.001
+        TBR = 2.9;
+    end 
     t_d_req = 2;
-    TBR = TBR_start;
-    I_s_0 = I_s_0_start;
-    f_b = TBE_array(i);
-    I_reserve = N_dot / f_b * q * t_res;
-    [out(i), I_startup(i), dummy, blanket_inventory(i), tes_inventory(i)] = utilities.find_tbr(I_s_0, I_reserve, t_d_req, TBR_start, model, TBR_accuracy, inventory_accuracy);
+%     TBR = TBR_start;
+%     I_s_0 = I_s_0_start;
+    I_reserve = N_dot / TBE * q * t_res;
+    [out(i), I_startup(i), ~,~,~] = utilities.find_tbr(I_s_0, I_reserve, t_d_req, TBR, model, TBR_accuracy, inventory_accuracy);
 end
 header = {'TBE [%]','TBR_req','I_startup [kg]'};
 writecell(header,strcat('results/TBE/TBE.csv'), "WriteMode","overwrite", 'Delimiter',','); % Use writecell because writematrix does not add the delimiter between strings
